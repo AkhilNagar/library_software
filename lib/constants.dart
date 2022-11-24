ac2bc838f17b12fef444318c4ae40b7d33ddac69
Galahad2819 <akhilnagar19@gmail.com>
2022-11-25 00:22:20 +0530

    Edited UI, Added books

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,58 @@ import 'package:flutter/material.dart';
 
 Color kPrimaryColor = Color(0xFFEC3133);
 Color kStarsColor = Color(0xFFFA6400);
+const FOOD_DATA = [
+  {
+    "Name":"Book 1",
+    "Author":"Hawkers",
+    "Rating":3,
+    "image":"ZeroToOne.jpg"
+  },{
+    "Name":"Book 1",
+    "Author":"Hawkers",
+    "Rating":3,
+    "image":"ZeroToOne.jpg"
+  },
+  {
+    "Name":"Book 1",
+    "Author":"Hawkers",
+    "Rating":3,
+    "image":"ZeroToOne.jpg"
+  },
+  {
+    "Name":"Book 1",
+    "Author":"Hawkers",
+    "Rating":3,
+    "image":"ZeroToOne.jpg"
+  },
+  {
+    "Name":"Book 1",
+    "Author":"Hawkers",
+    "Rating":3,
+    "image":"ZeroToOne.jpg"
+  },
+  {
+    "Name":"Book 1",
+    "Author":"Hawkers",
+    "Rating":3,
+    "image":"ZeroToOne.jpg"
+  },
+  {
+    "Name":"Book 1",
+    "Author":"Hawkers",
+    "Rating":3,
+    "image":"ZeroToOne.jpg"
+  },
+  {
+    "Name":"Book 1",
+    "Author":"Hawkers",
+    "Rating":3,
+    "image":"ZeroToOne.jpg"
+  },
+  {
+    "Name":"Book 1",
+    "Author":"Hawkers",
+    "Rating":3,
+    "image":"ZeroToOne.jpg"
+  }
+];
