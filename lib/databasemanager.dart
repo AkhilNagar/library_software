8509122532c25b18ff2379ec9ae99099d86fe60d
Galahad2819 <akhilnagar19@gmail.com>
2022-11-27 10:00:17 +0530

    Firebase Initialisation

--- lib/databasemanager.dart
+++ lib/databasemanager.dart
@@ -1,11 +1,51 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+final CollectionReference donatedList = FirebaseFirestore.instance.collection('donatedBooks');
+class Database {
+  static String? title;
+  static String? description;
+  static String? author;
+  static String? image;
+  static int? score;
+
+}
 class DatabaseManager {
-  final CollectionReference donatedList = FirebaseFirestore.instance.collection('donatedBooks');
+  static String? title;
+  static Future<void> addItem({
+    required String title,
+    required String description,
+    required String author,
+    required String image,
+    required int score,
 
-  Future<void> newDonate(String name, String bookName, String email, int number) async {
+  }) async {
+    DocumentReference documentReferencer =
+    donatedList.doc(title).collection('items').doc();
 
+    Map<String, dynamic> data = <String, dynamic>{
+      "title": title,
+      "description": description,
+      "author":author,
+      "image":image,
+      "score":score,
+    };
+
+    await documentReferencer
+        .set(data)
+        .whenComplete(() => print("Notes item added to the database"))
+        .catchError((e) => print(e));
   }
+
+
+
+
+
+  // final CollectionReference donatedList = FirebaseFirestore.instance.collection('donatedBooks');
+  //
+  // Future<void> newDonate(String name, String bookName, String email, int number) async {
+  //
+  // }
 
 
 }
