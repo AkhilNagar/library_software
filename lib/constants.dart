b28ce5129c44cdc28f7922d36f686703095a0fd5
hetvigudka28 <hetvigudka28@gmail.com>
2022-11-28 23:04:39 +0530

    Constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 Color kPrimaryColor = Color(0xff967259);
 Color kStarsColor = Color(0xFFA1887F);
-const FOOD_DATA = [
+var FOOD_DATA = [
   {
     "Name":"Book 1",
     "Author":"Hawkers",
